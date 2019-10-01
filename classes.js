4f54d82d8f88db734d419c0ce7c92975b7157c43
Otis <otisvickersgraver@gmail.com>
2019-10-01 18:05:48 +0100

    need to sort out node not recognizing es6 code in debugger

---
+++ classes.js
@@ -0,0 +1,7 @@
+export class Animal {
+    constructor(species, legs, tail) {
+        this.species = species;
+        this.legs = legs;
+        this.tail = tail;
+    }
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+import {Animal} from './classes.js'
+
+const human = new Animal('mammal', 2, false);
+
+console.log(human)
